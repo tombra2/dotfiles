01e5e304bac5d1e702c619027eab697b83532c67
Thomas Brandner <brandner.thomas@me.com>
2026-08-12 19:20:19 +0200

    nvim fix

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
