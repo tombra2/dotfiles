40efe9592b37c8d012cff764a6c335bd2b578257
Thomas Brandner <brandner.thomas@me.com>
2026-09-15 08:31:21 +0200

    feat(omarchy): add OmaSettings plugin and integration

---
+++ aether/.config/aether/theme/neovim.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		opts = {
+			variant = "main",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
+}
